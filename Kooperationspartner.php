b2e1b1f30652e3e0ebdfeb878433f62fb0155418
Konstantin Semenov <kvsemenoff@mail.ru>
2016-10-17 16:56:20 +0400

    d

---
+++ Kooperationspartner.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Kooperationspartner</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="libs/wow/animate.css">
+	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
+
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body class="qq-koorp">
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="libs/wow/wow.min.js"></script>
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+
+	<script>
+    	new WOW().init();
+    </script>
+
+	<script src="js/responsiveTabs.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/common.js"></script>
+
+	<section class="qq-impressum">
+		<div class="container">
+			<div class="row qq-top">
+				<div class="col-md-12">
+					<div class="col-md-4 col-sm-4 col-xs-4">
+						<span>Kooperationen</span>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-4 qq-top_center">
+						<a href="#"><img src="img/close-white.png" alt=""></a>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-4 qq-top_right">
+						<a href="#">
+							<img src="img/menu-white.png" alt="">
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 col-sm-12 col-xs-12 qq-txt">
+					<p>
+						Im sogenannten Premium-Wohnbereich steht Konstantin Wettig in seiner Funktion als Vorsitzender des Beirates der Engel & Völkers München GmbH auch nach dem Verkauf seines Unternehmens den geschätzten Kunden als erfahrener Ansprechpartner auf Wunsch zur Verfügung. 
+					</p>
+					<p>
+						Seine langjährige, operative Markterfahrung und Kenntnis des Münchner Immobilienmarktes kann somit weiterhin nutzenstiftend in Anspruch genommen werden. 
+					</p>
+				</div>
+			</div>
+			
+		</div>
+	</section>
+
+	
+</body>
+</html>
+
