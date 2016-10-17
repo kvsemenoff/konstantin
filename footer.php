07efcf2094082c3024d2dbb3a38016b81b38b5e5
Berzeg07 <berezg86@gmail.com>
2016-10-17 13:10:41 +0400

    17.10

--- footer.php
+++ footer.php
@@ -1,14 +1,89 @@
-<section class="footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-3">
-					<div class="ab-main-img-box">
-						<img src="img/art-1.png" alt="">
-						<span>Hhhhhhh</span>
-						<span></span>
-					</div>
-				</div>
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная</title>
 
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="libs/wow/animate.css">
+	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
+
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="libs/wow/wow.min.js"></script>
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+
+	<script>
+    	new WOW().init();
+    </script>
+
+	<script src="js/responsiveTabs.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/common.js"></script>
+
+	<?php require_once('includes/header.php'); ?>
+	
+	<?php require_once('includes/footer.php'); ?>
+
+<section class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="ab-main-img-box">
+					<img src="img/art-1.png" alt="">
+					<div class="ab-box-txt">
+						<span>Konstantin Wettig</span>
+						<span class="ab-box-txt-span">Immobilienscout</span>
+					</div>
+					<a href="#"><div class="ab-str"></div></a>
+				</div>
 			</div>
+			<div class="col-md-3">
+				<div class="ab-main-img-box">
+					<img src="img/art-2.png" alt="">
+					<div class="ab-box-txt">
+						<span>Leistungen</span>
+					</div>
+					<a href="#"><div class="ab-str"></div></a>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="ab-main-img-box">
+					<img src="img/art-3.png" alt="">
+					<div class="ab-box-txt">
+						<span>Geschäftspartner</span>
+					</div>
+					<a href="#"><div class="ab-str"></div></a>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="ab-main-img-box">
+					<img src="img/art-4.png" alt="">
+					<div class="ab-box-txt">
+						<span>Transaktions-<br>volumen</span>
+					</div>
+					<a href="#"><div class="ab-str"></div></a>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+
 		</div>
+	</div>
 </section>
+
+</body>
+</html>
+
+
+
