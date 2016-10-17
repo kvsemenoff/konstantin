316b92d15a7b3781ff020f473a2ce027114a83d8
Berzeg07 <berezg86@gmail.com>
2016-10-17 12:26:33 +0400

    17.10

---
+++ footer.php
@@ -0,0 +1,14 @@
+<section class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="ab-main-img-box">
+						<img src="img/art-1.png" alt="">
+						<span>Hhhhhhh</span>
+						<span></span>
+					</div>
+				</div>
+
+			</div>
+		</div>
+</section>
