8a11f32005ee79f29347e3924012f88c273e5b0d
Berzeg07 <berezg86@gmail.com>
2016-10-21 13:28:48 +0400

    17.10

---
+++ gescheftspartne.php
@@ -0,0 +1,147 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Главная</title>
+
+    <link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+    <link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+    <link rel="stylesheet" href="libs/wow/animate.css">
+    <link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
+
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <script src="libs/jquery/jquery-1.11.1.min.js"></script>
+    <script src="libs/owl.carousel/owl.carousel.js"></script>
+    <script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+    <script src="libs/wow/wow.min.js"></script>
+    <script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+    <script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+
+    <script>
+        new WOW().init();
+    </script>
+
+    <script src="js/responsiveTabs.js"></script>
+    <script src="js/jquery.maskedinput.min.js"></script>
+    <script src="js/common.js"></script>
+<div class="az-header1 az-margin-bottom45">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-xs-5">
+                <img class="img-responsive" src="img/ab-log.png" alt="">
+            </div>
+            <div class="col-md-2 col-xs-2 az-center">
+                <a href="/" class="az-close az-close_style az-close_style2"></a>
+            </div>
+            <div class="col-md-5 col-xs-5">
+                <div class="anz-menu">
+                    <input type="checkbox" id="check_1" class=""/>
+                    <label class="anz-menu-ch" for="check_1"></label>
+                    <div class="anz-perspective">
+                        <div class="menu-list-pressed">
+                            <label class="anz-menu-ch1" for="check_1"></label>
+                            <ul class="main-menu">
+                                <li><a href="/KonstantinWettig.php">KONSTANTIN</a></li>
+                                <li><a href="/leistungen.php">LEISTUNGEN</a></li>
+                                <li><a href="#">GESCHÄFTSPARTNER</a></li>
+                                <li><a href="/transaktionsvolumen.php">TRANSAKTIONSVOLUMEN</a></li>
+                                <li><a href="/werte.php">WERTE</a></li>
+                                <li><a href="/Kooperationspartner.php">KOOPERATIONSPARTNER</a></li>
+                                <li><a href="/presse.php">PRESSE</a></li>
+                                <li><a href="/kontakt.php">KONTAKT</a></li>
+                                <li><a href="/impressum.php">IMPRESSUM</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<section class="ab-logos">
+    <div class="container">
+        <div class="ab-logo-box box1">
+            <img class="img-responsive" src="img/BWG.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box2">
+            <img class="img-responsive" src="img/Bergerventure.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box3">
+            <img class="img-responsive" src="img/Binnberg.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box4">
+            <img class="img-responsive" src="img/Projektentwicklu.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box5">
+            <img class="img-responsive" src="img/Combine.jpg" alt="">
+        </div>
+        <div class="clearfix"></div>
+         <div class="ab-logo-box box6">
+            <img class="img-responsive" src="img/Sacher.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box7">
+            <img class="img-responsive" src="img/Hilmer.jpg" alt="">
+        </div>
+        <div class="clearfix"></div>
+        <div class="ab-logo-box box8">
+            <img class="img-responsive" src="img/Grundkontor.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box9">
+            <img class="img-responsive" src="img/DonnerReuschel.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box10">
+            <img class="img-responsive" src="img/Estate.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box11">
+            <img class="img-responsive" src="img/Heussen.jpg" alt="">
+        </div>
+        <div class="clearfix"></div>
+
+
+        <div class="ab-logo-box box12">
+            <img class="img-responsive" src="img/Hirmer.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box13">
+            <img class="img-responsive" src="img/Kefer.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box14">
+            <img class="img-responsive" src="img/Nachmann.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box15">
+            <img class="img-responsive" src="img/Oliv.jpg" alt="">
+        </div>
+        <div class="clearfix"></div>
+
+
+        <div class="ab-logo-box box16">
+            <img class="img-responsive" src="img/Gebhard.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box17">
+            <img class="img-responsive" src="img/kkconsulting.jpg" alt="">
+        </div>
+        <div class="clearfix"></div>
+
+        <div class="ab-logo-box box18">
+            <img class="img-responsive" src="img/Schoeller.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box19">
+            <img class="img-responsive" src="img/Immobilien.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box20">
+            <img class="img-responsive" src="img/DPDHL.jpg" alt="">
+        </div>
+        <div class="ab-logo-box box21">
+            <img class="img-responsive" src="img/Dr-Than.jpg" alt="">
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</section>
+</body>
+</html>
